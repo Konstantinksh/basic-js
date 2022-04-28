e6343ecfcea2089e4d9f8c57ed73f1cd308c10be
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-04-29 02:02:22 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,28 +4,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+ class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
+function isInputValid(input, arr) {
+  if (
+    !Number.isInteger(input) ||
+    input < 1 ||
+    input > arr.length
+  ) { 
+    return 'invalid';
+  }
+}
+
 const chainMaker = {
+  currentChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.currentChain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === null) {
+      this.currentChain[this.getLength()] = 'null';
+      return this;
+    } else {
+      this.currentChain[this.getLength()] = value;
+      return this;
+    }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isInputValid(position, this.currentChain) === 'invalid') {
+      this.currentChain = [];
+      throw new ValidationError('You can\'t remove incorrect link!')
+    };
+    this.currentChain.splice((position - 1), 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.currentChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = `( ${this.currentChain.join(' )~~( ')} )`;
+    this.currentChain = [];
+    return result
   }
 };
+
+
+// let test = [1, 2, 3];
+// let input = '2nd'
+// chainMaker.addLink(1).addLink(2).addLink( ).addLink(3).addLink(4).removeLink(1).reverseChain()
+// console.log(chainMaker.currentChain)
+// console.log(chainMaker.addLink(1).addLink(2).addLink( ).addLink(3).addLink(4).removeLink(1).reverseChain().finishChain())
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).addLink(4).removeLink(1).reverseChain().getLength())
+// console.log(chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain()) //'( 3rd )~~( function () { } )'
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0)
+// console.log(
+//   chainMaker.addLink(false).reverseChain().reverseChain().reverseChain().addLink({ 0: 'first', 1: 'second', 'length': 2 }).addLink(1.233).addLink(false).addLink(1).reverseChain().addLink(1).finishChain()
+// )
 
 module.exports = {
   chainMaker
