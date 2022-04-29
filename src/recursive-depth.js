5d2f47300121c8ceb7ca1c627d76daaac0560d0f
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-04-29 16:51:00 +0300

    feat: recursive in process of solving

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(arr) {
+    let resultDepth = 0; 
+    let currentDepth = 0;
+    goDown(arr, currentDepth, resultDepth);
+    
+    // return currentDepth
+  }  
 }
+
+
+function goDown(arr, currentDepth, resultDepth) { 
+  arr.forEach((element, index) => {
+    console.log(element)
+    if (isNotEmptyArray(element)){
+      ++currentDepth;
+      goDown(element, currentDepth, resultDepth)
+    } else if (Array.isArray(element)) {
+      ++currentDepth;
+    } else if (index === arr.length - 1) {
+      if (currentDepth > resultDepth) resultDepth = currentDepth;
+      return resultDepth
+    }
+    console.log('M ' + currentDepth + resultDepth)
+  });
+  // console.log([currentDepth, resultDepth])
+  }
+
+function isNotEmptyArray(arr) {
+  return (Array.isArray(arr) && arr.length > 0)
+}
+
+let test = [1, 2, 3, [1, 2, 3], 5, [1, 2, 3]]
+let dC = new DepthCalculator()
+console.log(dC.calculateDepth(test))
 
 module.exports = {
   DepthCalculator
