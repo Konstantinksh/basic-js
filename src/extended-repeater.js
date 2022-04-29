a7f2e48f91a83c4e29c659825fa05a115e838c05
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-04-29 23:20:27 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  let repeatTimes = options.repeatTimes;
+  let separator = getSeparator(options.separator);
+  let fullAddition = getFullAddition(options.addition, options.additionSeparator, options.additionRepeatTimes)
+  
+  let result = str + fullAddition;
+
+  if (Number.isInteger(repeatTimes) && repeatTimes>1) {
+    for (let i=0; i<repeatTimes-1; ++i) {
+      result += `${separator}${str}${fullAddition}`;    
+    }
+  }
+  console.log(result);
+  return result;
 }
+
+function getFullAddition(addition = '', additionSeparator = '|', additionRepeatTimes) {
+  let fullAdd = `${addition}`;
+  if (Number.isInteger(additionRepeatTimes) && additionRepeatTimes>1) {
+    for (let i=0; i<additionRepeatTimes-1; ++i) {
+      fullAdd += `${additionSeparator}${addition}`;      
+    }
+  }
+  console.log(fullAdd);
+  return fullAdd;
+}
+
+function getSeparator(separator = '+') {
+  separator = String(separator);
+  return separator    
+}
+
+repeater('STRING', { repeatTimes: 3, addition: 'PLUS', additionRepeatTimes: 2, })
+// => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
 
 module.exports = {
   repeater
